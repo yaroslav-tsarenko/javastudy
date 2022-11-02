6aa00f216f97c9980950bae3253245dd7bff8dab
Yaroslav <yaroslav7v@gmail.com>
2022-11-02 17:46:20 +0200

    added another methods of string format like a date, time, day of week

---
+++ studying/src/main/java/org/example/string/methods/AllDescriptionDateTime.java
@@ -0,0 +1,9 @@
+package org.example.string.methods;
+
+import java.util.Date;
+
+public class AllDescriptionDateTime {
+    public static void main(String[] args) {
+        System.out.println(String.format("%tc", new Date()));
+    }
+}
---
+++ studying/src/main/java/org/example/string/methods/JustTimeStringFormat.java
@@ -0,0 +1,9 @@
+package org.example.string.methods;
+
+import java.util.Date;
+
+public class JustTimeStringFormat {
+    public static void main(String[] args) {
+        System.out.println(String.format("%tr", new Date()));
+    }
+}
