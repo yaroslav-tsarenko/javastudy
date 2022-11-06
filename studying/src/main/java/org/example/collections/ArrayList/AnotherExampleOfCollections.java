73489df46f34ab74af7a2dfd9ffe25d3cfce849c
Yaroslav <yaroslav7v@gmail.com>
2022-11-06 23:41:53 +0200

    developed collection framework

---
+++ studying/src/main/java/org/example/collections/ArrayList/AnotherExampleOfCollections.java
@@ -0,0 +1,23 @@
+package org.example.collections.ArrayList;
+
+import java.util.ArrayList;
+
+public class AnotherExampleOfCollections {
+    public static void main(String[] args) {
+        ArrayList<String> people = new ArrayList<>();
+        people.add("John");
+        people.add("Dave");
+        people.add("Jane");
+
+        people.remove("Dave");
+        people.remove(1);
+
+        if(people.contains("John")){
+            System.out.println("John is in the list");
+        }
+
+        if(people.isEmpty()){
+            System.out.println("The list is empty!");
+        }
+    }
+}
