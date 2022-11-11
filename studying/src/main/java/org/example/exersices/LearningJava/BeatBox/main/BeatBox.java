c9f895857042eb52f6e997e82b0d92e547f11eba
Yaroslav <yaroslav7v@gmail.com>
2022-11-11 14:10:41 +0200

    fixed BeatBox program and added readafile example

--- studying/src/main/java/org/example/exersices/LearningJava/BeatBox/main/BeatBox.java
+++ studying/src/main/java/org/example/exersices/LearningJava/BeatBox/main/BeatBox.java
@@ -16,11 +16,11 @@ public class BeatBox {
     Track track;
     JFrame theFrame;
 
-    String[] instrumentNames = {"Bass Drum", "Closed Hit-Hat", "Open Hi-Hat", "Acoustic Share", "Crash Cymbal", "Hand Clap" +
-            "High Tom", "Hi Bongo", "Maracas", "Whistle", "Low Conga", "Cowbell", "Vibraslap", "Low-mid Tom", "High Agogo" +
+    String[] instrumentNames = {"Bass Drum", "Closed Hit-Hat", "Open Hi-Hat", "Acoustic Share", "Crash Cymbal", "Hand Clap",
+            "High Tom", "Hi Bongo", "Maracas", "Whistle", "Low Conga", "Cowbell", "Vibraslap", "Low-mid Tom", "High Agogo",
             "Open Hi Conga"};
 
-    int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 64, 56, 58, 47, 67, 63};
+    int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
 
     public static void main(String[] args) {
         new BeatBox().buildGUI();
