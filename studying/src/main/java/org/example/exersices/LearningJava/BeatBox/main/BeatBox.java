14d1e6e835a8cab9edce89bf08e36e10b6e587db
Yaroslav <yaroslav7v@gmail.com>
2022-11-12 23:34:28 +0200

    create dailyAdviceClient

--- studying/src/main/java/org/example/exersices/LearningJava/BeatBox/main/BeatBox.java
+++ studying/src/main/java/org/example/exersices/LearningJava/BeatBox/main/BeatBox.java
@@ -213,7 +213,6 @@ public class BeatBox {
     public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick){
         MidiEvent event = null;
         try{
-
             ShortMessage a = new ShortMessage();
             a.setMessage(comd, chan, one, two);
             event = new MidiEvent(a, tick);
