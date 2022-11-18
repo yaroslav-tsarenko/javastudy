4ff30eb5b4e8b7a30fd8ec74233afb80c2829b10
Yaroslav <yaroslav7v@gmail.com>
2022-11-18 23:51:19 +0200

    added examples of LinkedList

---
+++ studying/src/main/java/org/example/collections/LinkedList/ExampleOfLinkedList.java
@@ -0,0 +1,41 @@
+package org.example.collections.LinkedList;
+
+import java.util.LinkedList;
+
+public class ExampleOfLinkedList {
+    public static void main(String[] args) {
+
+        String str1 = new String("Hello World!");
+        String str2 = new String("My name is Earl");
+        String str3 = new String("I love Java");
+        String str4 = new String("I live in Ukraine");
+
+        LinkedList<String> earlBio = new LinkedList<>();
+        earlBio.add(str1);
+        earlBio.add(str2);
+        earlBio.add(str3);
+        earlBio.add(str4);
+
+        System.out.println(earlBio);
+
+    }
+}
+
+class ElementAddInTheMiddleOfList{
+    public static void main(String[] args) {
+
+        String str1 = new String("Hello World!");
+        String str2 = new String("My name is Earl");
+        String str3 = new String("I love Java");
+        String str4 = new String("I live in Ukraine");
+
+        LinkedList<String> earlBio = new LinkedList<>();
+        earlBio.add(str1);
+        earlBio.add(str3);
+        earlBio.add(1, str2);
+
+        System.out.println(earlBio);
+
+    }
+
+}
