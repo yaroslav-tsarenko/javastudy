fcc8555484904d1321c0faa9b617d5c0b22ece19
Yaroslav <yaroslav7v@gmail.com>
2022-11-20 23:29:07 +0200

    started learning about java io and nio

---
+++ studying/src/main/java/org/example/ionio/InputStream.java
@@ -0,0 +1,21 @@
+package org.example.ionio;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class InputStream {
+    public static void main(String[] args) {
+        try{
+            
+            FileInputStream fin = new FileInputStream("D:/javaRush/file.txt");
+
+            System.out.printf("File size: %d bytes \n", fin.available());
+            int i = -1;
+            while ((i = fin.read()) != -1) {
+                System.out.print((char) i);
+            }
+        } catch(IOException exception){
+            System.out.println(exception.getMessage());
+        }
+    }
+}
