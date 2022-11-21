6c1a4edddd7a7706d02296d27f658ad19c1e34b2
Yaroslav <yaroslav7v@gmail.com>
2022-11-21 16:27:52 +0200

    added JavaBeans constructor

---
+++ studying/src/main/java/org/example/exersices/LearningJava/JavaBeansConstructor/MainNutritionFacts.java
@@ -0,0 +1,13 @@
+package org.example.exersices.LearningJava.JavaBeansConstructor;
+
+public class MainNutritionFacts {
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new NutritionFacts();
+        cocaCola.setServingSize(240);
+        cocaCola.setServings(8);
+        cocaCola.setCalories(100);
+        cocaCola.setSodium(35);
+        cocaCola.setCarbohydrate(27);
+        System.out.println(cocaCola);
+    }
+}
---
+++ studying/src/main/java/org/example/exersices/LearningJava/JavaBeansConstructor/NutritionFacts.java
@@ -0,0 +1,47 @@
+package org.example.exersices.LearningJava.JavaBeansConstructor;
+
+public class NutritionFacts {
+    private int servingSize = -1;
+    private int servings = -1;
+
+    private int calories = 0;
+    private int fat = 0;
+    private int sodium = 0;
+    private int carbohydrate = 0;
+
+    public void setServingSize(int val) {
+        servingSize = val;
+    }
+
+    public void setServings(int val) {
+        servings = val;
+    }
+
+    public void setCalories(int val) {
+        calories = val;
+    }
+
+    public void setFat(int val) {
+        fat = val;
+    }
+
+    public void setSodium(int val) {
+        sodium = val;
+    }
+
+    public void setCarbohydrate(int carbohydrate) {
+        this.carbohydrate = carbohydrate;
+    }
+
+    @Override
+    public String toString() {
+        return "NutritionFacts{" +
+                "servingSize=" + servingSize +
+                ", servings=" + servings +
+                ", calories=" + calories +
+                ", fat=" + fat +
+                ", sodium=" + sodium +
+                ", carbohydrate=" + carbohydrate +
+                '}';
+    }
+}
