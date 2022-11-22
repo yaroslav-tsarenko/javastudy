d465b0ef26c0a8c787566541298e72439462557e
Yaroslav <yaroslav7v@gmail.com>
2022-11-22 23:04:49 +0200

    file copy for nio and nio2

---
+++ studying/src/main/java/org/example/javaIoAndNio/NIO2/FileCopy.java
@@ -0,0 +1,24 @@
+package org.example.javaIoAndNio.NIO2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileCopy {
+    public static void main(String[] args) {
+        long currentMills = System.currentTimeMillis();
+        long startMills = currentMills;
+
+        Path sourceDirectory = Paths.get("C:/Users/IdeaProjects/testFolder/test.txt");
+        Path targetDirectory = Paths.get("C:/Users/IdeaProjects/testFolder/test3.txt");
+        try {
+            Files.copy(sourceDirectory, targetDirectory);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        currentMills = System.currentTimeMillis();
+        System.out.println("Время выполнения в миллисекундах: " + (currentMills - startMills));
+    }
+}
