1eea6ef4d1bf57a9c490509cf1508c32c0b63af0
Yaroslav <yaroslav7v@gmail.com>
2022-11-23 22:31:52 +0200

    learning about reflection

---
+++ studying/src/main/java/org/example/reflection/MyClass.java
@@ -0,0 +1,67 @@
+package org.example.reflection;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
+public class MyClass {
+
+    private int number;
+    private String name = "default";
+
+    //    public MyClass(int number, String name) {
+//        this.number = number;
+//        this.name = name;
+//    }
+
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    private void printData(){
+        System.out.println(number + name);
+    }
+
+    public static void main(String[] args) {
+        MyClass myClass = null;
+        try {
+            Class clazz = Class.forName(MyClass.class.getName());
+            myClass = (MyClass) clazz.newInstance();
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        System.out.println(myClass);//output created object reflection.MyClass@60e53b93
+    }
+}
+
+class creatingMyClassSpecimen {
+    public static void main(String[] args) {
+        MyClass myClass = null;
+        Class clazz = null;
+        try {
+            clazz = Class.forName(MyClass.class.getName());
+            Class[] params = {int.class, String.class};
+            myClass = (MyClass) clazz.getConstructor(params).newInstance(1, "default2");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException |
+                 InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        System.out.println(myClass);//output created object reflection.MyClass@60e53b93
+
+
+        //get constructor from class
+        Constructor[] constructors = clazz.getConstructors();
+        for (Constructor constructor : constructors) {
+            Class[] paramTypes = constructor.getParameterTypes();
+            for (Class paramType : paramTypes) {
+                System.out.print(paramType.getName() + " ");
+            }
+            System.out.println();
+        }
+    }
+}
