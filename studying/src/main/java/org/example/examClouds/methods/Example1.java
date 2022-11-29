389327d5819c7e26687fea23116b194795934f61
Yaroslav <yaroslav7v@gmail.com>
2022-11-29 21:56:43 +0200

    complete working with methods

--- studying/src/main/java/org/example/examClouds/methods/Example1.java
+++ studying/src/main/java/org/example/examClouds/methods/Example1.java
@@ -12,7 +12,7 @@ public class Example1 {
 
 class Example2 {
     public static void main(String[] args) {
-      int x =  sum(3,4);
+        int x = sum(3, 4);
         System.out.println(x);
 
     }
@@ -21,4 +21,38 @@ class Example2 {
     static int sum(int a, int b) {
         return (a + b);
     }
+}
+
+
+class Example3 {
+
+    /*Метод getVolume принимает на вход три параметра типа double, а также возвращает значение типа double.
+    Метод возвращает значение с помощью ключевого слова return:*/
+
+    public static void main(String[] args) {
+        double result = getVolume(4.5,5.5,7.7);
+        System.out.println(result);
+    }
+
+    static double getVolume(double width, double height, double depth) {
+        return width * height * depth;
+    }
+}
+
+class Example4{
+    /*Объявление и вызов метода Java*/
+        public static void main(String[] args) {
+            int x, y;
+            x = square(5);
+            System.out.println(x);
+            x = square(9);
+            System.out.println(x);
+            y = 2;
+            x = square(y);
+            System.out.println(x);
+        }
+
+        public static int square(int i) {
+            return i * i;
+        }
 }
