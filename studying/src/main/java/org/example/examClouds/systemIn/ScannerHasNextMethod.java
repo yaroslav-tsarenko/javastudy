06aeb899b75327e9c984a28773f30f5b569b86b0
Yaroslav <yaroslav7v@gmail.com>
2022-11-30 22:41:14 +0200

    worked with scanners

---
+++ studying/src/main/java/org/example/examClouds/systemIn/ScannerHasNextMethod.java
@@ -0,0 +1,52 @@
+package org.example.examClouds.systemIn;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ScannerHasNextMethod {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter whole number: ");
+        if (scanner.hasNextInt()) {
+            int i = scanner.nextInt();
+            System.out.println(i);
+        } else {
+            System.out.println("You entered not whole number");
+        }
+    }
+}
+
+class ScannerDoubleMethod{
+    public static void main(String[] args) {
+        while(true){
+            try{
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter number: ");
+                double userInput = scanner.nextDouble();
+                System.out.println(userInput);
+                if (userInput == scanner.nextDouble()){
+                    break;
+                }
+                // если ввести букву s,
+                // то случится ошибка во время исполнения
+            }catch (InputMismatchException e){
+                System.err.println("Enter number not a word");
+            }
+        }
+    }
+}
+
+class ScannerNextLineMethod{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter something: ");
+        scanner.nextLine();
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Enter something again: ");
+        scanner1.nextLine();
+        String s1, s2;
+        s1 = scanner.nextLine();
+        s2 = scanner1.nextLine();
+        System.out.println(s1 + s2);
+    }
+}
---
+++ studying/src/main/java/org/example/examClouds/variableRefactoring/Test.java
@@ -0,0 +1,9 @@
+package org.example.examClouds.variableRefactoring;
+
+public class Test {
+    public static void main(String[] vars) {
+        long var1 = 130L;
+        byte var2 = (byte) var1;
+        System.out.println("Byte is " + var2);
+    }
+}
