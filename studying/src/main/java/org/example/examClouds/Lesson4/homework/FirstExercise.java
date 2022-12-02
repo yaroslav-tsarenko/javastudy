3aac199f3aaf7a79dc8f5ef074e8d82f683780bd
Yaroslav <yaroslav7v@gmail.com>
2022-12-02 23:46:14 +0200

    added second exercise

--- studying/src/main/java/org/example/examClouds/Lesson4/homework/FirstExercise.java
+++ studying/src/main/java/org/example/examClouds/Lesson4/homework/FirstExercise.java
@@ -1,18 +1,46 @@
 package org.example.examClouds.Lesson4.homework;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class FirstExercise {
     public static void main(String[] args) {
-        while (true){
+        while (true) {
             Scanner scannerNumber = new Scanner(System.in);
             System.out.println("Enter whole number: ");
             int userInput = scannerNumber.nextInt();
-            if (userInput % 2 == 0){
+            if (userInput % 2 == 0) {
                 System.out.println("number is even");
-            }else {
+            } else {
                 System.out.println("number is not even");
             }
         }
+    }
+}
+
+class SecondExercise {
+    public static void main(String[] args) throws InterruptedException {
+        SecondExercise secondExercise = new SecondExercise();
+        secondExercise.start();
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter three numbers: ");
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+        c = Math.abs(c);
+
+        System.out.println(a + " " + b + " " + c);
+
+        double tmp = Math.min(a,b);
+        double min = Math.min(tmp, c);
+
+        System.out.println("Min number with module:\n" + min);
+
     }
 }
