1ac157aaec29dde2d44d9523b086240175adfa1e
Yaroslav <yaroslav7v@gmail.com>
2022-12-05 15:38:16 +0200

    completed homework

---
+++ studying/src/main/java/org/example/examClouds/Lesson5/homework/GarlandVersionTwo.java
@@ -0,0 +1,63 @@
+package org.example.examClouds.Lesson5.homework;
+
+
+import java.util.Scanner;
+
+public class GarlandVersionTwo {
+
+    public static void main(String[] args) {
+        GarlandVersionTwo garlandVersionTwo = new GarlandVersionTwo();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number from 1 to 4, " +
+                "method 1 - blink, method 2 - check first lamp, method 3 - run line, " +
+                "method 4 - state of the garland");
+        int userInput = scanner.nextInt();
+        switch (userInput) {
+            case 1:
+                garlandVersionTwo.blink();
+                break;
+            case 2:
+                garlandVersionTwo.isLampOn();
+                break;
+            case 3:
+                garlandVersionTwo.run();
+                break;
+            case 4:
+                 garlandVersionTwo.stateOfTheGarland();
+                break;
+        }
+    }
+
+    public void blink() {
+        for (int j = 0; j < 1; j++) {
+            int[] garland = new int[32];
+            Integer.toBinaryString(garland.length);
+            for (int i = 0; i < garland.length; i++) {
+                System.out.println("lamp blinked");
+            }
+        }
+    }
+
+
+    public void run() {
+        int[] Garland = new int[32];
+        for (int i = 0; i < Garland.length; i++) {
+            System.out.println("lamp blinked");
+            System.out.println("lamp off");
+        }
+    }
+
+    public void isLampOn() {
+        String s = "lamp blinked";
+        String d = "lamp off";
+        if (s.equals("lamp blinked")) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+
+    public void stateOfTheGarland(){
+        System.out.println("lamps on");
+    }
+}
---
+++ studying/src/main/java/org/example/examClouds/Lesson5/homework/PrintSymbol.java
@@ -0,0 +1,9 @@
+package org.example.examClouds.Lesson5.homework;
+
+public class PrintSymbol {
+    public static void main(String[] args) {
+        for (char i = 'h'; i <= 'l'; i++) {
+            System.out.println(i);
+        }
+    }
+}
