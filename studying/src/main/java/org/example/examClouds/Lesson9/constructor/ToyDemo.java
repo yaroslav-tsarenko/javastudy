8fd840629b85f3e56132f6699b584770db006c5f
Yaroslav <yaroslav7v@gmail.com>
2022-12-10 00:59:01 +0200

    added toy constructor

---
+++ studying/src/main/java/org/example/examClouds/Lesson9/constructor/ToyDemo.java
@@ -0,0 +1,7 @@
+package org.example.examClouds.Lesson9.constructor;
+
+public class ToyDemo {
+    public static void main(String[] args) {
+        Toy toy = new Toy("Doll", 34, "Disney", 3);
+    }
+}
