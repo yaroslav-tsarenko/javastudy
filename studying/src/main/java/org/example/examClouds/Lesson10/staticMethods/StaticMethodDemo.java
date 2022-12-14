b9398b2779cef13ce491144b0c8d3d85721cdb1d
Yaroslav <yaroslav7v@gmail.com>
2022-12-14 20:15:17 +0200

    added static methods

---
+++ studying/src/main/java/org/example/examClouds/Lesson10/staticMethods/StaticMethodDemo.java
@@ -0,0 +1,7 @@
+package org.example.examClouds.Lesson10.staticMethods;
+
+public class StaticMethodDemo {
+    public static void main(String[] args) {
+        StaticMethodClass.staticMethod(42);
+    }
+}
