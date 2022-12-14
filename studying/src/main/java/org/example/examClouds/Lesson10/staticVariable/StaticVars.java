acbcb53e61820d8840d87e20711ac3ba178fd804
Yaroslav <yaroslav7v@gmail.com>
2022-12-14 19:56:54 +0200

    added static variable

---
+++ studying/src/main/java/org/example/examClouds/Lesson10/staticVariable/StaticVars.java
@@ -0,0 +1,12 @@
+package org.example.examClouds.Lesson10.staticVariable;
+
+public class StaticVars {
+    int a;
+   static int b;
+
+    public static void main(String[] args) {
+        StaticVars staticVars = new StaticVars();
+        System.out.println(staticVars.a);
+        System.out.println(b);
+    }
+}
