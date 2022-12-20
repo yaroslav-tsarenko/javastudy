dad2eee170359b037e0aa850a53159f81f0f5ca7
Yaroslav <yaroslav7v@gmail.com>
2022-12-20 18:38:13 +0200

    added formatter

---
+++ studying/src/main/java/org/example/examClouds/Lesson13/formatter/FormatDemo4.java
@@ -0,0 +1,8 @@
+package org.example.examClouds.Lesson13.formatter;
+
+public class FormatDemo4 {
+    public static void main(String[] args) {
+        System.out.printf("Строка c форматированием: %.2f%n", 1000.0 / 3.0);
+        System.out.format("%s, в следующем году вам будет %d", "Джон", 23);
+    }
+}
