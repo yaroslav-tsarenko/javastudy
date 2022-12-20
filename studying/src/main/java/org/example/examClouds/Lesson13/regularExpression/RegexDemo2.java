7083c8195561d66e3aff3469803570db04303fb4
Yaroslav <yaroslav7v@gmail.com>
2022-12-20 19:54:05 +0200

    added regex

---
+++ studying/src/main/java/org/example/examClouds/Lesson13/regularExpression/RegexDemo2.java
@@ -0,0 +1,13 @@
+package org.example.examClouds.Lesson13.regularExpression;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexDemo2 {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("a*b");
+        Matcher matcher = pattern.matcher("aaab");
+        boolean b = matcher.matches();
+        System.out.println(b);
+    }
+}
