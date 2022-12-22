939e2048b42c4ef6a3aea604fb3d1b472270444b
Yaroslav <yaroslav7v@gmail.com>
2022-12-22 13:07:45 +0200

    added number format

---
+++ studying/src/main/java/org/example/examClouds/Lesson14/numberFormat/NumberFormatDemo1.java
@@ -0,0 +1,29 @@
+package org.example.examClouds.Lesson14.numberFormat;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class NumberFormatDemo1 {
+    public static void main(String[] args) {
+        double number = 123.4567;
+        Locale locFR = new Locale("fr");
+
+        NumberFormat numberFormat1 = NumberFormat.getInstance();
+        System.out.println("Число в текущей локали: " + numberFormat1.format(number));
+
+        NumberFormat numberFormat2 = NumberFormat.getInstance(locFR);
+        System.out.println("Число во французской локали: " + numberFormat2.format(number));
+
+        NumberFormat numberFormat3 = NumberFormat.getCurrencyInstance();
+        System.out.println("Денежная единица в текущей локали: " + numberFormat3.format(number));
+
+        NumberFormat numberFormat4 = NumberFormat.getCurrencyInstance(locFR);
+        System.out.println("Денежная единица во французской локали: " + numberFormat4.format(number));
+
+        NumberFormat numberFormat5 = NumberFormat.getPercentInstance();
+        System.out.println("Процент в текущей локали: " + numberFormat5.format(number));
+
+        NumberFormat numberFormat6 = NumberFormat.getPercentInstance(locFR);
+        System.out.println("Процент во французской локали: " + numberFormat6.format(number));
+    }
+}
