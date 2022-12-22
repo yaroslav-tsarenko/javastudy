7d20eb9cca2b0f6db13e557a956e7d0741a05ced
Yaroslav <yaroslav7v@gmail.com>
2022-12-22 12:59:48 +0200

    added locale classes and locale resources

---
+++ studying/src/main/java/org/example/examClouds/Lesson14/locale/ResourceBundleDemo2.java
@@ -0,0 +1,25 @@
+package org.example.examClouds.Lesson14.locale;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ResourceBundleDemo2 {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        printInfo("", "");
+
+        printInfo("en", "US");
+        printInfo("uk", "UA");
+    }
+
+    private static void printInfo(String language, String country)
+            throws UnsupportedEncodingException {
+        Locale current = new Locale(language, country);
+        ResourceBundle rb = ResourceBundle.getBundle("text", current);
+        for (String key : rb.keySet()) {
+            String value = rb.getString(key);
+            System.out.println(value);
+        }
+        System.out.println();
+    }
+}
