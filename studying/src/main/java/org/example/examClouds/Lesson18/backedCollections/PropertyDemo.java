145202c856ec429c1f9a3358d303dd9260b5b2c4
Yaroslav <yaroslav7v@gmail.com>
2023-01-02 12:13:55 +0200

    added backed collections

---
+++ studying/src/main/java/org/example/examClouds/Lesson18/backedCollections/PropertyDemo.java
@@ -0,0 +1,32 @@
+package org.example.examClouds.Lesson18.backedCollections;
+
+import java.util.Properties;
+import java.util.Set;
+
+public class PropertyDemo {
+    public static void main(String[] args) {
+        Properties capitals = new Properties();
+
+        capitals.put("Illinois", "Springfield");
+        capitals.put("Missouri", "Jefferson City");
+        capitals.put("Washington", "Olympia");
+        capitals.put("California", "Sacramento");
+        capitals.put("Indiana", "Indianapolis");
+
+        // Get a set-view of the keys.
+        Set states = capitals.keySet();
+
+        // Show all states and capitals.
+        for (Object name : states) {
+            System.out.println("The capital of " + name + " is "
+                    + capitals.getProperty((String) name) + ".");
+        }
+
+        System.out.println();
+
+        // Look for state not in list -- specify default.
+        String str = capitals.getProperty("Florida", "Not Found");
+        System.out.println("The capital of Florida is "
+                + str + ".");
+    }
+}
