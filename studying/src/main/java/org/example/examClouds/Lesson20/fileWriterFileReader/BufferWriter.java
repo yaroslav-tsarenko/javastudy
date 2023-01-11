da2c64dd8d26bbf96bff26708a8a2eca3e910b4c
Yaroslav <yaroslav7v@gmail.com>
2023-01-11 12:49:48 +0200

    explored file reader and file writer

--- studying/src/main/java/org/example/examClouds/Lesson20/fileWriterFileReader/BufferWriter.java
+++ studying/src/main/java/org/example/examClouds/Lesson20/fileWriterFileReader/BufferWriter.java
@@ -7,10 +7,14 @@ import java.io.IOException;
 public class BufferWriter {
     public static void main(String[] args) throws IOException {
         BufferedWriter buff = new BufferedWriter(new FileWriter("studying\\hello.txt"));
-        String s = "Hello Java";
-        for (int i = 0; i < 5; i++) {
-            System.out.println(" ");
-            buff.write(s);
+        String s = "Hello Java!";
+        for (int i = 0; i < 1; i++) {
+            for (int j = 0; j < 1; j++) {
+                for (int k = 0; k < 1; k++) {
+                    buff.write(s);
+                    buff.append("\n");
+                }
+            }
         }
         buff.close();
     }
