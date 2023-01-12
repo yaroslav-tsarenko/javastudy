3a6bb5967943b62337582453ff6a47fdf119c04e
Yaroslav <yaroslav7v@gmail.com>
2023-01-12 22:36:25 +0200

    explored shallowCopying

---
+++ studying/src/main/java/org/example/examClouds/Lesson22/shallowCopying/CloneCarDemo.java
@@ -0,0 +1,16 @@
+package org.example.examClouds.Lesson22.shallowCopying;
+
+public class CloneCarDemo {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Car car = new Car("Грузовик", new Driver("Василий", 45));
+        Car clonedCar = car.clone();
+        System.out.println("Оригинал:\t" + car);
+        System.out.println("Клон:   \t" + clonedCar);
+
+        Driver clonedCarDriver = clonedCar.getDriver();
+        clonedCarDriver.setName("Вася");
+
+        System.out.println("Оригинал после изменения имени водителя:\t" + car);
+        System.out.println("Клон после изменения имени водителя:   \t\t" + clonedCar);
+    }
+}
