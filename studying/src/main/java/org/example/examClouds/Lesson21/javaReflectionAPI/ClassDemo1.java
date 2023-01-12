80406db84fb5beae7559d30007322a193395433e
Yaroslav <yaroslav7v@gmail.com>
2023-01-12 21:52:58 +0200

    added another examples from java API

---
+++ studying/src/main/java/org/example/examClouds/Lesson21/javaReflectionAPI/ClassDemo1.java
@@ -0,0 +1,29 @@
+package org.example.examClouds.Lesson21.javaReflectionAPI;
+
+public class ClassDemo1 {
+    public static void main(String[] args) {
+        getClassName1();
+        getClassName2();
+        getClassName3();
+    }
+
+    private static void getClassName1() {
+        String s = "Это строка";
+        Class aClass = s.getClass();
+        System.out.println(aClass);
+    }
+
+    private static void getClassName2() {
+        try {
+            Class aClass = Class.forName("java.lang.String");
+            System.out.println(aClass);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void getClassName3() {
+        Class aClass = String.class;
+        System.out.println(aClass);
+    }
+}
