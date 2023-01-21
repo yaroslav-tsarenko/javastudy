3b0aad44cdc46e21f59fdcbde97c94671277180b
Yaroslav <yaroslav7v@gmail.com>
2023-01-21 14:20:34 +0200

    exploring data base sql

---
+++ studying/src/main/java/org/example/examClouds/Lesson23/JavaDataBase/UserDAO.java
@@ -0,0 +1,68 @@
+package org.example.examClouds.Lesson23.JavaDataBase;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO extends AbstractDAO<Integer, User> {
+    public static final String SQL_SELECT_ALL_USERS = "SELECT * FROM users";
+    public static final String SQL_SELECT_USER_ID =
+            "SELECT * FROM users WHERE id=?";
+
+    @Override
+    public List<User> findAll() {
+        List<User> users = new ArrayList<>();
+        DriverManager ConnectorDB = null;
+        try (Connection connection = ConnectorDB.getConnection();
+             Statement statement = connection.createStatement()) {
+            ResultSet rs = statement.executeQuery(SQL_SELECT_ALL_USERS);
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                boolean add = users.add(new User(id, name));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return users;
+    }
+
+    @Override
+    public User findEntityById(Integer id) {
+        User user = null;
+        DriverManager ConnectorDB;
+        try (Connection connection = ConnectorDB.getConnection();
+             PreparedStatement statement =
+                     connection.prepareStatement(SQL_SELECT_USER_ID)) {
+            statement.setInt(1, id);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                String name = rs.getString(2);
+                user = new User(id, name);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return user;
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean delete(User entity) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean create(User entity) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public User update(User entity) {
+        throw new UnsupportedOperationException();
+    }
+}
