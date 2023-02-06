4e4c97a3f1732cc1cbecf0f33aac5d4d33ec85ef
Yaroslav <yaroslav7v@gmail.com>
2023-02-06 20:58:59 +0200

    created model for http

--- studying/src/main/java/org/example/httpProtocol/model/User.java
+++ studying/src/main/java/org/example/httpProtocol/model/User.java
@@ -1,12 +1,18 @@
 package org.example.httpProtocol.model;
 
+import java.util.Objects;
+
 public class User {
     private Long id;
     private String name;
+    private String email;
+    private String phoneNumber;
 
-    public User(Long id, String name) {
+    public User(Long id, String name, String email, String phoneNumber) {
         this.id = id;
         this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
     }
 
     public User(){
@@ -27,5 +33,41 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+
+        if (!Objects.equals(id, user.id)) return false;
+        if (!Objects.equals(name, user.name)) return false;
+        if (!Objects.equals(email, user.email)) return false;
+        return Objects.equals(phoneNumber, user.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (phoneNumber != null ? phoneNumber.hashCode() : 0);
+        return result;
     }
 }
