c2e2077e2cc0fb5c5fbdd292969d94247a1b5d30
Yaroslav <yaroslav7v@gmail.com>
2023-02-11 22:32:13 +0200

    added third example into two sum

--- studying/src/main/java/org/example/leetCode/TwoSum.java
+++ studying/src/main/java/org/example/leetCode/TwoSum.java
@@ -1,6 +1,7 @@
 package org.example.leetCode;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Supplier;
 
 public class TwoSum {
@@ -17,7 +18,7 @@ public class TwoSum {
         }
 
         TwoSum twoSum = new TwoSum();
-        System.out.println(twoSum.twoSum(array, target));
+        twoSum.twoSumSecond(array, target);
     }
 
     public int[] twoSum(int[] numbers, int target) {
@@ -29,5 +30,34 @@ public class TwoSum {
             map.put(numbers[i], i);
         }
         return null;
+    }
+
+    public int[] twoSumSecond(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] == target - numbers[i]) {
+                    return new int[]{i, j};
+
+                }
+            }
+        }
+        throw new IllegalArgumentException("No solution for defined input data");
+    }
+
+    public int[] twoSumThird(int[] numbers, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            map.put(numbers[i], i);
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            int requiredNumber = target - numbers[i];
+            if (map.containsKey(requiredNumber) && map.get(requiredNumber) != i) {
+                return new int[]{i, map.get(requiredNumber)};
+            }
+        }
+        throw new IllegalArgumentException("No solution for defined input data!");
+
     }
 }
