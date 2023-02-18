78a42f3c313c63fa3cfd0777417c0a30abbaf470
yaroslav-tsarenko <yaroslav7v@gmail.com>
2023-02-18 22:17:02 +0200

    discovered how to delete from the anywhere of array

---
+++ studying/src/main/java/org/example/leetCode/DeletingFromTheEndOfArray.java
@@ -0,0 +1,26 @@
+package org.example.leetCode;
+
+public class DeletingFromTheEndOfArray {
+    public static void main(String[] args) {
+
+        // Declare an integer array of 10 elements.
+        int[] intArray = new int[10];
+
+        // The array currently contains 0 elements
+        int length = 0;
+
+        // Add elements at the first 6 indexes of the Array.
+        for(int i = 0; i < 6; i++) {
+            intArray[length] = i;
+            length++;
+        }
+
+        // Deletion from the end is as simple as reducing the length
+        // of the array by 1.
+        length--;
+
+        for (int i = 0; i < length; i++) {
+            System.out.println("Index " + i + " contains " + intArray[i]);
+        }
+    }
+}
