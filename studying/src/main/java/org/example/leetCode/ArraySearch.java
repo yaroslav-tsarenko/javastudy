b51162e2a8a39a44219814c01d0824a9f536a34c
yaroslav-tsarenko <yaroslav7v@gmail.com>
2023-02-21 20:46:16 +0200

    learned about linear search in array

---
+++ studying/src/main/java/org/example/leetCode/ArraySearch.java
@@ -0,0 +1,43 @@
+package org.example.leetCode;
+
+public class ArraySearch {
+    public static void main(String[] args) {
+
+        // Declare a new array of 6 elements
+        int[] array = new int[6];
+
+        // Variable to keep track of the length of the array
+        int length = 0;
+
+        // Iterate through the 6 indexes of the Array.
+        for (int i = 0; i < 6; i++) {
+            // Add a new element and increment the length as well
+            array[length++] = i;
+        }
+
+        // Print out the results of searching for 4 and 30.
+        System.out.println("Does the array contain the element 4? - " + ArraySearch.linearSearch(array, length, 4));
+        System.out.println("Does the array contain the element 30? - " + ArraySearch.linearSearch(array, length, 30));
+
+        // Does the array contain the element 4? - true
+        // Does the array contain the element 30? - false
+    }
+
+    public static boolean linearSearch(int[] array, int length, int element) {
+        // Check for edge cases
+        if (array == null || length == 0) {
+            return false;
+        }
+
+        // Check each element starting from the first one
+        for (int i = 0; i < length; i++) {
+            // We found the element at index i, so return true.
+            if (array[i] == element) {
+                return true;
+            }
+        }
+
+        // We didn't find the element in the array.
+        return false;
+    }
+}
