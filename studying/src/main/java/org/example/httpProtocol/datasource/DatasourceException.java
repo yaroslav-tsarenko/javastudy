d938a8edc562d155e098f49b676691196653e535
yaroslav-tsarenko <yaroslav7v@gmail.com>
2023-02-24 23:22:05 +0200

    added datasource exception to jdbc

---
+++ studying/src/main/java/org/example/httpProtocol/datasource/DatasourceException.java
@@ -0,0 +1,7 @@
+package org.example.httpProtocol.datasource;
+
+public class DatasourceException extends RuntimeException{
+    public DatasourceException(String message) {
+        super(message);
+    }
+}
