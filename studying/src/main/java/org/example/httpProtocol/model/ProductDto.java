c7a29f261f8a6e59328b11195c8be5171d5b9cc9
yaroslav-tsarenko <yaroslav7v@gmail.com>
2023-03-04 20:31:48 +0200

    created product dto

---
+++ studying/src/main/java/org/example/httpProtocol/model/ProductDto.java
@@ -0,0 +1,34 @@
+package org.example.httpProtocol.model;
+
+public class ProductDto {
+
+    private Long id;
+
+    private String name;
+
+    private String seller;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+}
