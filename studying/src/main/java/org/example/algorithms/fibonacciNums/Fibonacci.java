d905afc6f38926bbf44f8e5517929396947990ab
yaroslav-tsarenko <yaroslav7v@gmail.com>
2023-03-05 20:04:39 +0200

    learned about difficulty of algorithms

--- studying/src/main/java/org/example/algorithms/fibonacciNums/Fibonacci.java
+++ studying/src/main/java/org/example/algorithms/fibonacciNums/Fibonacci.java
@@ -12,6 +12,7 @@ public class Fibonacci {
         System.out.println(fibWithInt(n));
     }
 
+    // O(2^n)
     private static long fibNaive(int n, long[] memoization) {
         if (memoization[n] != -1)
             return memoization[n];
@@ -23,24 +24,27 @@ public class Fibonacci {
         return result;
     }
 
+    // O(n)
     private static long fibEffective(int n) {
-        long[] arr = new long[n + 1];
+        long[] arr = new long[n + 1]; // O(n)
 
-        arr[0] = 0;
-        arr[1] = 1;
+        arr[0] = 0;// O(1)
+        arr[1] = 1;// O(1)
 
-        for (int i = 2; i <= n; i++)
+        for (int i = 2; i <= n; i++) // O(n)
             arr[i] = arr[i - 1] + arr[i - 2];
-        return arr[n];
+        return arr[n]; // O(1)
+
+        // O(n) + O(1) + O(1) + O(1) + O(n) = O(2n) + O(3) = O(2n + 3) = O(n)
     }
 
-    private static int fibWithInt(int n){
+    private static int fibWithInt(int n) {
         int a = 1;
         int b = 0;
-        if(n < 2){
+        if (n < 2) {
             return n;
         }
-        for(int i = 2; i<=n;i++){
+        for (int i = 2; i <= n; i++) {
             int c = a + b;
             b = a;
             a = c;
