9f4d35b711c2c9ffb29d82caf739f6617e7ae3ba
yaroslav-tsarenko <yaroslav7v@gmail.com>
2023-03-12 23:50:07 +0200

    edited sorted list to binary search

--- studying/src/main/java/org/example/leetCode/SortedListToBinarySearch.java
+++ studying/src/main/java/org/example/leetCode/SortedListToBinarySearch.java
@@ -4,22 +4,23 @@ public class SortedListToBinarySearch {
     public static void main(String[] args) {
 
     }
+
     public TreeNode sortedListToBST(ListNode head) {
-        if(head==null)
+        if (head == null)
             return null;
-        if(head.next==null)
+        if (head.next == null)
             return new TreeNode(head.val);
-        ListNode slow=head;
-        ListNode fast=head.next.next;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        TreeNode res=new TreeNode(slow.next.val);
-        ListNode righthalf=slow.next.next;
-        slow.next=null;
-        res.left=sortedListToBST(head);
-        res.right=sortedListToBST(righthalf);
+        TreeNode res = new TreeNode(slow.next.val);
+        ListNode rightHalf = slow.next.next;
+        slow.next = null;
+        res.left = sortedListToBST(head);
+        res.right = sortedListToBST(rightHalf);
         return res;
     }
 }
