b291800308728d11c28a0b603f602aad2d5553db
yaroslav-tsarenko <yaroslav7v@gmail.com>
2023-03-12 23:50:16 +0200

    solve valid parentheses

---
+++ studying/src/main/java/org/example/leetCode/ValidParentheses.java
@@ -0,0 +1,25 @@
+package org.example.leetCode;
+
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
