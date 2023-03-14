faf4c52426b2115051d444f67043ba201a9f1a84
yaroslav-tsarenko <yaroslav7v@gmail.com>
2023-03-14 11:57:08 +0200

    solve LongestCommonPrefix

---
+++ studying/src/main/java/org/example/leetCode/LongestCommonPrefix.java
@@ -0,0 +1,27 @@
+package org.example.leetCode;
+
+import org.example.statements.arrays.Array;
+
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String strings[] = {"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefix(strings));
+    }
+
+    public static String longestCommonPrefix(String strs[]) {
+        Arrays.sort(strs);
+        String str1 = strs[0];
+        String str2 = strs[strs.length - 1];
+        int index = 0;
+        while (index < str1.length()) {
+            if (str1.charAt(index) == str2.charAt(index)) {
+                index++;
+            } else {
+                break;
+            }
+        }
+        return index == 0 ? "" : str1.substring(0, index);
+    }
+}
