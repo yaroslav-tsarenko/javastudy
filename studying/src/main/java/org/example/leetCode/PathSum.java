52f0085da0e57b35166c01ed8a6ffba5ff17abab
yaroslav-tsarenko <yaroslav7v@gmail.com>
2023-03-19 23:54:29 +0200

    solve path sum

---
+++ studying/src/main/java/org/example/leetCode/PathSum.java
@@ -0,0 +1,16 @@
+package org.example.leetCode;
+
+public class PathSum {
+    public static void main(String[] args) {
+
+    }
+   public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.val == targetSum && root.left == null && root.right == null) {
+            return true;
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
