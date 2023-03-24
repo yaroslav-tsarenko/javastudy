77781dbb0591cc9c89b0e2f23c968b0bd0c246df
yaroslav-tsarenko <yaroslav7v@gmail.com>
2023-03-24 20:56:33 +0200

    solve inorder traversal

--- studying/src/main/java/org/example/leetCode/InorderTraversal.java
+++ studying/src/main/java/org/example/leetCode/InorderTraversal.java
@@ -1,13 +1,14 @@
 package org.example.leetCode;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class InorderTraversal {
     public static void main(String[] args) {
 
     }
 
-    List<Integer> arr = new List<>();
+    List<Integer> arr = new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
         if (root!=null) {
             inorderTraversal(root.left);
