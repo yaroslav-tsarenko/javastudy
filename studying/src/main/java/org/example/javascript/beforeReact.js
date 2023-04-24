fa2d9fad57796358395555b83a6b06f622cec93b
yaroslav <yaroslav7v@gmail.com>
2023-04-24 23:34:39 +0300

    started learning react

--- studying/src/main/java/org/example/javascript/beforeReact.js
+++ studying/src/main/java/org/example/javascript/beforeReact.js
@@ -29,3 +29,56 @@ import smear from './smear.js';
 console.log(smear1('physics'));
 console.log(smear('math'));
 
+import {checkLS, showMessage} from "./cookies";
+
+if (!checkLS()) console.log(showMessage('Problem'));
+
+function functionName() {
+
+}
+
+const a1 = (b, c) => b * 4 * c;
+console.log(a1(3, 2))
+
+// spread rest
+// ...
+
+const normalWork = ["engineer", "worker", "mechanic"];
+const work = [...normalWork, "bloger", "inst model"];
+console.log(work);
+
+const worker = {
+    "name": "Alex",
+    "job": "bloger"
+}
+
+const macDuk = {
+    ...worker,
+    "position": "clean mananger"
+}
+
+console.log(macDuk)
+
+//rest
+const num = (...args) => {
+    console.log(args);
+}
+
+console.log(num(1, 2));
+console.log(num(1, 2, 77, 88, 99));
+
+// desctucturization
+
+const arr = [33, 44, 55, 66, 77];
+const [d1, ,, d2] = arr;
+console.log(d1, d2);
+
+const {name, position} = macDuk;
+console.log(name, position);
+
+// map, filter
+//
+
+
+
+
