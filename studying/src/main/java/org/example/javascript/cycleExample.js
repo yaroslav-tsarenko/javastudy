69068f20cadab6990627d4b9958eeeb4c2ec8e59
yaroslav <yaroslav7v@gmail.com>
2023-08-18 23:02:43 +0300

    continue learning javascript

--- studying/src/main/java/org/example/javascript/cycleExample.js
+++ studying/src/main/java/org/example/javascript/cycleExample.js
@@ -3,4 +3,5 @@ let arr1 = [5, 7, 3, 8, 9, 2];
 for (let i = 0; i < arr1.length; i++) {
     arr1[i] *= 2;
     console.log("Element " + i + ": " + arr1[i]);
+    console.log("Element " + i + ": " + arr1[i]);
 }
