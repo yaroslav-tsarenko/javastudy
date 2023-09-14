4c7fe88d1ee1e4fe59ad248f0e685c4b435da3c2
yaroslav <yaroslav7v@gmail.com>
2023-09-14 23:40:08 +0300

    exploring useState hook

--- studying/src/main/java/org/example/react/studying-react-project/src/App.js
+++ studying/src/main/java/org/example/react/studying-react-project/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+function computeInitialCounter() {
+    console.log('Some calculations...')
+    return Math.trunc(Math.random() * 20)
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    /* const [counter, setCounter] = useState(0);
+     const [counter, setCounter] = useState(computeInitialCounter);*/
+
+    const [counter, setCounter] = useState(() => {
+        return computeInitialCounter()
+    })
+
+    function increment() {
+        setCounter((prevCounter) => {
+            return prevCounter + 1
+        })
+        setCounter(prev => prev + 1)
+    }
+
+    function decrement() {
+        setCounter(counter - 1)
+    }
+
+    return (
+        <div>
+            <h1>Counter: {counter}</h1>
+            <button onClick={increment} className="btn btn-success">Add</button>
+            <button onClick={decrement} className="btn btn-danger">Remove</button>
+        </div>
+    );
 }
 
 export default App;
