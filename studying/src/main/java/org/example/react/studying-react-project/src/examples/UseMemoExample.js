237f07097b3911592c063b15d0bdd49db764b171
yaroslav <yaroslav7v@gmail.com>
2023-09-16 23:57:31 +0300

    exploring useMemo

---
+++ studying/src/main/java/org/example/react/studying-react-project/src/examples/UseMemoExample.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+function UseMemoExample() {
+
+    const [number, setNumber] = useState(42)
+
+    return (
+        <>
+         <h1></h1>
+        </>
+    );
+};
+
+export default UseMemoExample;
