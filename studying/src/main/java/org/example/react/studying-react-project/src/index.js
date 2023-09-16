9f9b52aaae9c167109c567cfad4656b1084a42f1
yaroslav <yaroslav7v@gmail.com>
2023-09-16 14:47:54 +0300

    added components to index.js

--- studying/src/main/java/org/example/react/studying-react-project/src/index.js
+++ studying/src/main/java/org/example/react/studying-react-project/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './examples/App';
 import reportWebVitals from './reportWebVitals';
+import UseEffectExample from "./examples/UseEffectExample";
+import UseRefExample from "./examples/UseRefExample";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+        <UseEffectExample/>
+        <UseRefExample/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
