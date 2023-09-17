ee3aa39e960652c541b3031708889454ed446914
yaroslav <yaroslav7v@gmail.com>
2023-09-17 23:50:36 +0300

    exploring useMemo

--- studying/src/main/java/org/example/react/studying-react-project/src/examples/UseMemoExample.js
+++ studying/src/main/java/org/example/react/studying-react-project/src/examples/UseMemoExample.js
@@ -1,12 +1,34 @@
-import React, {useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
+
+function complexCompute(num) {
+    let i = 0
+    while (i < 100) i++
+    return num * 2
+}
 
 function UseMemoExample() {
 
     const [number, setNumber] = useState(42)
+    const [colored, setColored] = useState(false)
+
+    const styles = {
+        color: colored ? 'darkred' : 'black'
+    }
+
+    const computed = useMemo(() => {
+        return complexCompute(number)
+    }, [number])
+
+    useEffect(() => {
+        console.log('Styles changed')
+    }, [styles]);
 
     return (
         <>
-         <h1></h1>
+            <h1 style={styles}>Computed property: {computed}</h1>
+            <button className={"btn btn-success"} onClick={() => setNumber(prev => prev + 1)}>Add</button>
+            <button className={"btn btn-danger"} onClick={() => setNumber(prev => prev - 1)}>Remove</button>
+            <button className={"btn btn-warning"} onClick={() => setNumber(prev => prev - 1)}>Change</button>
         </>
     );
 };
