e303d1863ae46b4e5e471120c1f8122cd1c34988
yaroslav <yaroslav7v@gmail.com>
2023-09-25 22:19:12 +0300

    explored useRef

--- studying/src/main/java/org/example/react/studying-react-project/src/examples/UseRefExample.js
+++ studying/src/main/java/org/example/react/studying-react-project/src/examples/UseRefExample.js
@@ -2,19 +2,26 @@ import React, {useEffect, useRef, useState} from 'react';
 
 
 function UseRefExample() {
-   /* const [renderCounter, setRenderCount] = useState(1)*/
-    const [value, setValue] = useState('initial')
-    const renderCount = useRef(1)
-
+    /* const [renderCounter, setRenderCount] = useState(1)*/
+    const [value, setValue] = useState('initial');
+    const renderCount = useRef(1);
+    const inputRef = useRef(null);
+    const prevValue = useRef('');
     useEffect(() => {
         renderCount.current++
     });
 
+    useEffect(() => {
+        prevValue.current = value
+    }, [value]);
+
+
     return (
         <div>
             <h1>Amount of renders: {renderCount.current}</h1>
-            <input type="text" onChange={e => setValue(e.target.value)} value={value}/>
+            <input type="text" ref={inputRef} onChange={e => setValue(e.target.value)} value={value}/>
         </div>
+
     )
 }
 
