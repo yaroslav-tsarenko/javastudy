b65dbd5fa0922c007f04add9936303e88020f6c5
yaroslav <yaroslav7v@gmail.com>
2023-10-02 20:38:17 +0300

    refactored index.js

--- studying/src/main/java/org/example/react/studying-react-project/src/index.js
+++ studying/src/main/java/org/example/react/studying-react-project/src/index.js
@@ -9,6 +9,7 @@ import UseMemoExample from "./examples/UseMemoExample";
 import Button from "./examples/Button";
 import UseCallbackExample from "./examples/UseCallbackExample";
 import UseContextExample from "./examples/useContextExample";
+import Popup from "./examples/Popup";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -20,10 +21,7 @@ root.render(
         <Button/>
         <UseCallbackExample/>
         <UseContextExample/>
+        <Popup/>
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
